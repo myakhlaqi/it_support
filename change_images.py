c55642757fe97b6f3ba4ed7af35ce6d8f6a16e0a
Mohammad <csmaster90@hotmail.com>
2021-11-14 01:02:13 +0800

    Add new script for changing and converting images

--- change_images.py
+++ change_images.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-
+import os
 from PIL import Image
 
 
-im = Image("example.jpg")
-im.rotate(180).resize((640,480)).save("flipped_and_resized.jpg")
+def get_image_paths(mypath):
+    return [ os.path.abspath(os.path.join(mypath, f)) for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    
+
+#print(onlyfiles)
+#os.mkdir("./new_images")
+def convert_image(image_path_list, save_path):
+    for image_path in image_path_list:
+        pic = Image.open(image_path)
+        new_pic=os.path.abspath(os.path.join(save_path,os.path.basename(image_path)))
+        pic.rotate(-90).resize((128,128)).convert("RGB").save(new_pic,"JPEG")
+
+convert_image(get_image_paths("./image"), "./image/new_images")
