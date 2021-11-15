05672fb807c40cb9e688b81611bdaced1827e4ac
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2021-11-15 22:15:13 +0800

    Update change_images.py

--- change_images.py
+++ change_images.py
@@ -6,7 +6,6 @@ def get_image_paths(mypath):
     return [ os.path.abspath(os.path.join(mypath, f)) 
     for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) and f.startswith("ic_")]
 
-
 def convert_image(image_path_list, save_path):
     for image_path in image_path_list:
         pic = Image.open(image_path)
