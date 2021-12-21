4fc466f9bf81d2a0398b3b26ebf5a6e0434fa4e5
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2021-12-22 00:28:38 +0800

    Update cars.py

--- SendEmailWithAttachment/scripts/cars.py
+++ SendEmailWithAttachment/scripts/cars.py
@@ -18,7 +18,6 @@ def format_car(car):
   return "{} {} ({})".format(
       car["car_make"], car["car_model"], car["car_year"])
 
-
 def process_data(data):
   """Analyzes the data, looking for maximums.
 
